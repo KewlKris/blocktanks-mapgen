2fd28cfce46aea94c0a8bef010bbf6a505bce53e
Christopher Chamberlain <chambca@gmail.com>
2022-02-07 13:35:43 -0600

    Add basic test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-import TileMap from './tilemap.js';
+import {TileMap} from './tilemap.js';
+import CanvasManager from './canvasmanager.js';
 
 async function init() {
-    
+    let canvasManager = new CanvasManager(document.getElementById('map-canvas'));
+    let map = new TileMap(20, 40);
+    canvasManager.setMap(map);
+    canvasManager.drawMap();
+    canvasManager.updateMainCanvas();
 }
 
 // Initialize now or when the page finishes loading
