a3733a5fedf570b55ba02cf94e8578a014487a93
Christopher Chamberlain <chambca@gmail.com>
2022-02-07 23:10:00 -0600

    Add recording to TileMap

--- js/tilemap.js
+++ js/tilemap.js
@@ -1,3 +1,6 @@
+import Tile from './tile.js';
+import TileMapAnimation from './tilemapanimation.js';
+
 class TileMap {
     /** @type {Number} */
     #width;
@@ -8,6 +11,9 @@ class TileMap {
     /** @type {Tile[][]} */
     #tileMap;
 
+    /** @type {TileMapAnimation} */
+    animation;
+
     /**
      * Initialize a new TileMap
      * @param {Number} width 
@@ -16,6 +22,7 @@ class TileMap {
     constructor(width, height) {
         this.#width = width;
         this.#height = height;
+        this.animation = new TileMapAnimation();
 
         // Initialize the tile map
         this.#tileMap = [];
@@ -25,6 +32,7 @@ class TileMap {
                 let tileType = (x == 0 || x == width - 1 || y == 0 || y == height - 1) ? 'wall' : 'empty';
                 let tile = new Tile(this, tileType, x, y);
                 if (tileType == 'wall') tile.addProperty('immutable');
+                tile.setRecordable(true);
                 row.push(tile);
             }
             this.#tileMap.push(row);
@@ -39,6 +47,17 @@ class TileMap {
      */
     getTile(x, y) {
         return this.#tileMap[y][x];
+    }
+
+    /**
+     * Add this Tile to the TileMap and remove the previous Tile in its place
+     * @param {Tile} tile 
+     */
+    addTile(tile) {
+        let {x, y} = tile.getCoords();
+        this.#tileMap[y][x] = tile;
+
+        this.animation.logTileUpdate(tile);
     }
 
     /**
@@ -75,154 +94,4 @@ class TileMap {
     }
 }
 
-class Tile {
-    /**
-     * @typedef {'empty' | 'wall' | 'fence'} TileType
-     */
-
-    /**
-     * @typedef {{x: Number, y: Number}} TileCoordinates
-     */
-
-    /** @type {TileType} */
-    #tileType;
-
-    /** @type {String[]} */
-    #properties;
-
-    /** @type {TileMap} */
-    #map;
-
-    /** @type {TileCoordinates} */
-    #coords;
-
-    /**
-     * Initialize a new Tile
-     * @param {TileMap} map
-     * @param {TileType} tileType
-     * @param {Number} x
-     * @param {Number} y 
-     */
-    constructor(map, tileType, x, y) {
-        this.#map = map;
-        this.#tileType = tileType;
-        this.#properties = [];
-        this.#coords = {x, y};
-        
-        this.addProperty('untouched'); // Mark all tiles as untouched when created
-    }
-
-    /**
-     * Add a property to this Tile
-     * @param {String} property 
-     */
-    addProperty(property) {
-        if (this.#properties.indexOf(property) == -1) {
-            this.#properties.push(property);
-        }
-    }
-
-    /**
-     * Check whether this Tile has the given property
-     * @param {String} property 
-     * @returns {Boolean}
-     */
-    hasProperty(property) {
-        return (this.#properties.indexOf(property) != -1);
-    }
-
-    /**
-     * Remove a property from this Tile
-     * @param {String} property 
-     */
-    removeProperty(property) {
-        let index = this.#properties.indexOf(property);
-        if (index != -1) {
-            this.#properties.splice(index, 1);
-        }
-    }
-
-    /**
-     * Change the Tile's type
-     * @param {TileType} tileType 
-     */
-    setType(tileType) {
-        this.#tileType = tileType;
-        this.removeProperty('untouched');
-    }
-
-    /**
-     * Get the Tile's type
-     * @returns {TileType}
-     */
-    getType() {
-        return this.#tileType;
-    }
-
-    /**
-     * Get the Tile's coordinates on the map
-     * @returns {TileCoordinates}
-     */
-    getCoords() {
-        return this.#coords;
-    }
-
-    get tileLeft() {
-        return this.#getTileFromOffset(-1, 0);
-    }
-
-    get TileRight() {
-        return this.#getTileFromOffset(1, 0);
-    }
-
-    get tileAbove() {
-        return this.#getTileFromOffset(0, -1);
-    }
-
-    get tileBelow() {
-        return this.#getTileFromOffset(0, 1);
-    }
-
-    get tileTopLeft() {
-        return this.#getTileFromOffset(-1, -1);
-    }
-
-    get tileTopRight() {
-        return this.#getTileFromOffset(1, -1);
-    }
-
-    get tileBottomLeft() {
-        return this.#getTileFromOffset(-1, 1);
-    }
-
-    get tileBottomRight() {
-        return this.#getTileFromOffset(1, 1);
-    }
-
-    /**
-     * Get the Tile which is at an offset from this one
-     * @param {Number} xOffset 
-     * @param {Number} yOffset 
-     * @returns {Tile | Undefined}
-     */
-    #getTileFromOffset(xOffset, yOffset) {
-        let {x, y} = this.#coords;
-        x += xOffset;
-        y += yOffset;
-
-        let width = this.#map.getWidth();
-        let height = this.#map.getHeight();
-
-        if (x < 0 || x >= width || y < 0 || y >= height) {
-            // Out of range!
-            return undefined;
-        }
-
-        return this.#map.getTile(x, y);
-    }
-}
-
-export {
-    TileMap,
-    Tile
-};
+export default TileMap;
